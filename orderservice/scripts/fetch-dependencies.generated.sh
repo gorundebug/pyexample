de13c5aceed368dc2cd99006b168ba196866709e
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-31 20:31:14 +0300

    Regenerate for framework release v0.2.51

--- orderservice/scripts/fetch-dependencies.generated.sh
+++ orderservice/scripts/fetch-dependencies.generated.sh
@@ -64,6 +64,6 @@ fetch_module() {
   fi
 }
 
-fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.50" "inventory_service_api"
-fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.50" "model_python"
-fetch_module "order_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.50" "order_service_api"
+fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.51" "inventory_service_api"
+fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.51" "model_python"
+fetch_module "order_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.51" "order_service_api"
--- scripts/python/setup.generated.sh
+++ scripts/python/setup.generated.sh
@@ -14,7 +14,7 @@ if [[ ! -d .pyservicelib/.git ]]; then
     echo ".pyservicelib exists but is not a Git checkout" >&2
     exit 1
   fi
-  git clone --branch "v0.2.50" \
+  git clone --branch "v0.2.51" \
     --depth 1 \
     "https://github.com/gorundebug/pyservicelib.git" \
     .pyservicelib
