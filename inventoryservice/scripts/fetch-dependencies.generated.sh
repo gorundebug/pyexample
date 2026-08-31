34d679ed88f919e9ec2ceaf4910ec4ce71ea2dff
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-08-31 03:53:53 +0300

    Regenerate for framework release v0.2.33

--- inventoryservice/scripts/fetch-dependencies.generated.sh
+++ inventoryservice/scripts/fetch-dependencies.generated.sh
@@ -64,5 +64,5 @@ fetch_module() {
   fi
 }
 
-fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.32" "inventory_service_api"
-fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.32" "model_python"
+fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.33" "inventory_service_api"
+fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.33" "model_python"
--- orderservice/scripts/fetch-dependencies.generated.sh
+++ orderservice/scripts/fetch-dependencies.generated.sh
@@ -64,6 +64,6 @@ fetch_module() {
   fi
 }
 
-fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.32" "inventory_service_api"
-fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.32" "model_python"
-fetch_module "order_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.32" "order_service_api"
+fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.33" "inventory_service_api"
+fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.33" "model_python"
+fetch_module "order_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.33" "order_service_api"
