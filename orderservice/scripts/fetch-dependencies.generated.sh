c5d38aa30f3fed8ef9488123fdd61f922a419c83
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-02 01:18:34 +0300

    Regenerate for framework release v0.2.62

--- orderservice/scripts/fetch-dependencies.generated.sh
+++ orderservice/scripts/fetch-dependencies.generated.sh
@@ -64,6 +64,6 @@ fetch_module() {
   fi
 }
 
-fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.61" "inventory_service_api"
-fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.61" "model_python"
-fetch_module "order_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.61" "order_service_api"
+fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.62" "inventory_service_api"
+fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.62" "model_python"
+fetch_module "order_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.62" "order_service_api"
