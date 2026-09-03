282a65e3bd8fde8993b5518c194bc78a0ac79266
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-03 03:17:37 +0300

    Regenerate for framework release v0.2.76

--- automationservice/scripts/fetch-dependencies.generated.sh
+++ automationservice/scripts/fetch-dependencies.generated.sh
@@ -64,4 +64,4 @@ fetch_module() {
   fi
 }
 
-fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.75" "model_python"
+fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.76" "model_python"
--- inventoryservice/scripts/fetch-dependencies.generated.sh
+++ inventoryservice/scripts/fetch-dependencies.generated.sh
@@ -64,5 +64,5 @@ fetch_module() {
   fi
 }
 
-fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.75" "inventory_service_api"
-fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.75" "model_python"
+fetch_module "inventory_service_api" "https://github.com/gorundebug/pyexample.git" "v0.2.76" "inventory_service_api"
+fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.76" "model_python"
