01bf8b7d47037b5b58f842253049f49b0c114998
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-05 02:11:19 +0300

    Regenerate for framework release v0.2.84

--- scripts/python/setup.generated.sh
+++ scripts/python/setup.generated.sh
@@ -16,7 +16,7 @@ if [[ ! -d .pyservicelib/.git ]]; then
     echo ".pyservicelib exists but is not a Git checkout" >&2
     exit 1
   fi
-  git_retry git clone --branch "v0.2.83" \
+  git_retry git clone --branch "v0.2.84" \
     --depth 1 \
     "https://github.com/gorundebug/pyservicelib.git" \
     .pyservicelib
