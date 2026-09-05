5cd8b3ad09db9f6d1fa6d020151e63be4a7336d8
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-05 05:26:01 +0300

    Release v0.2.86

--- analyticsservice/scripts/fetch-dependencies.generated.sh
+++ analyticsservice/scripts/fetch-dependencies.generated.sh
@@ -64,4 +64,4 @@ fetch_module() {
   fi
 }
 
-fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.85" "model_python"
+fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.86" "model_python"
