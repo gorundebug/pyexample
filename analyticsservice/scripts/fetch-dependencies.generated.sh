010a73ce5db9c0e58b0556669eebfc30efa14daa
Sergey Alexeev <sergeyalexeev@yahoo.com>
2026-09-24 00:48:21 +0300

    Regenerate for framework release v0.2.135

--- analyticsservice/scripts/fetch-dependencies.generated.sh
+++ analyticsservice/scripts/fetch-dependencies.generated.sh
@@ -64,4 +64,4 @@ fetch_module() {
   fi
 }
 
-fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.134" "model_python"
+fetch_module "model_python" "https://github.com/gorundebug/pyexample.git" "v0.2.135" "model_python"
